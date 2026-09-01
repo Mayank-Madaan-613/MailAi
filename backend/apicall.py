90a647a132b62056aa25cfb4241f0adb97d6843e
Mayank <mayankmadaan613@gmail.com>
2026-09-01 12:19:48 +0530

    chore: enhaced prompt for api call

--- backend/apicall.py
+++ backend/apicall.py
@@ -6,10 +6,10 @@ import json
 load_dotenv()
 api_key=os.getenv("gemini_api_key")
 class Prompt:
-    def __init__(self,detail:dict):
+    def __init__(self):
         self.gem_obj=genai.Client(api_key=api_key)
     def generate(self,detail:dict):
-        self.prompt=f"{detail} these are the details of a mail, now give me the summary and a reply draft for this mail in json format and return nothing except json"
+        self.prompt=f"{detail} these are the details of a mail, now give me the summary and ask minimum questions for drafting a reply for this mail(return json like 'q1':'ques...')"
         response=self.gem_obj.models.generate_content(model="gemini-3-flash-preview",contents=self.prompt, config=types.GenerateContentConfig(response_mime_type="application/json"))
         out=response.text
-        self.result=json.loads(out)
+        return out
